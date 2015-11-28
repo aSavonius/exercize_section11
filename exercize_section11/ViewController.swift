a8e74b79f568ec13e74d2aa1381f47ad787fef29
Andrea Di Stefano <andrea.distefano@me.com>
2015-11-29 00:18:46 +0100

    Added the files of the exercise

--- exercize_section11/ViewController.swift
+++ exercize_section11/ViewController.swift
@@ -9,7 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // objects in the storybiard must be declarated before the "viewdidload" function as @IBOutlet
+    @IBOutlet weak var redSquare: UIImageView!
+    @IBOutlet weak var blueSquare: UIImageView!
+    @IBOutlet weak var hideBlueButton: UIButton!
+    @IBOutlet weak var hideRedButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +26,20 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // actions must be after "didReceiveMemoryWarning" function and any objetc that make an action must have previously declared as @IBOutlet above
+    
+    @IBAction func hideBlueSquare(sender: AnyObject) {
+            blueSquare.hidden=true
+    }
 
-
+    @IBAction func hideRedSquare(sender: AnyObject) {
+            redSquare.hidden=true
+    }
+    
+    @IBAction func resetAll(sender: AnyObject) {
+            blueSquare.hidden=false
+            redSquare.hidden=false
+    }
 }
 
